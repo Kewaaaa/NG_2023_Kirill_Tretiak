f440ad788d7cac358a272d2137e06a15994bb021
Kewaaaa <inokenty119228@gmail.com>
2023-04-05 13:44:19 +0300

    Complete with task 4

---
+++ Lection_2/Task_4/main.py
@@ -0,0 +1,143 @@
+print("Menu:\n1.Add book.\n2.Delete book.\n3.Edit book.\n4.Find book.")
+choise = int(input("Choose the action[choose number]: "))
+if choise < 0 or choise > 4:
+    print("You chose wrons action!")
+library = {
+    "Book_1": {
+        "Title": "Sherlock Holmes",
+        "Author": "Arthur Conan Doyle",
+        "Pages": "1392",
+        "Genre": "Crime, Thriller & Mystery",
+        "Book binding": "Soft"
+    },
+
+    "Book_2": {
+        "Title": "Harry Potter",
+        "Author": "Joanne Rowling",
+        "Pages": "300",
+        "Genre": "Fantasy",
+        "Book binding": "Hard"
+    },
+
+    "Book_3": {
+        "Title": "Biblia",
+        "Author": "Jesus",
+        "Pages": "666",
+        "Genre": "Fantasy, thriller",
+        "Book binding": "Hard"
+    }
+
+}
+
+bufferDict = {}
+count = len(library) + 1
+if choise == 1:
+    while True:
+        name = input("Enter the title of the book: ")
+        bufferDict["Title"] = name
+        author = input("Enter the author: ")
+        bufferDict["Author"] = author
+        page = input("Enter numer of pages: ")
+        bufferDict["Pages"] = page
+        genre = input("Enter a genre of book: ")
+        bufferDict["Genre"] = genre
+        binding = input("Enter a boob binding (soft/hard): ")
+        bufferDict["Book binding"] = binding
+        getAnswer = input("If you add all books please type 'exit': ")
+        newKey = f"Book_{count}"
+        count += 1
+        library[newKey] = bufferDict
+        bufferDict = {}
+        if getAnswer == 'exit':
+            break
+    for books, booksInfo in library.items():
+        print(books)
+        # I used this construction all time, because according to the task, i can't use function
+        booksName = booksInfo["Title"]
+        # It shows all books in better view
+        authorName = booksInfo["Author"]
+        booksPages = booksInfo["Pages"]
+        booksGenre = booksInfo["Genre"]
+        booksBinding = booksInfo["Book binding"]
+        print(f"Title - {booksName}")
+        print(f"Author - {authorName}")
+        print(f"Pages - {booksPages}")
+        print(f"Genre - {booksGenre}")
+        print(f"Binding - {booksBinding}")
+
+
+if choise == 2:
+    for books, booksInfo in library.items():
+        print(books)
+        booksName = booksInfo["Title"]
+        authorName = booksInfo["Author"]
+        booksPages = booksInfo["Pages"]
+        booksGenre = booksInfo["Genre"]
+        booksBinding = booksInfo["Book binding"]
+        print(f"Title - {booksName}")
+        print(f"Author - {authorName}")
+        print(f"Pages - {booksPages}")
+        print(f"Genre - {booksGenre}")
+        print(f"Binding - {booksBinding}")
+    deleteBook = input("Which book you want to delete[number of book]: ")
+    del library[f"Book_{deleteBook}"]
+    for books, booksInfo in library.items():
+        print(books)
+        booksName = booksInfo["Title"]
+        authorName = booksInfo["Author"]
+        booksPages = booksInfo["Pages"]
+        booksGenre = booksInfo["Genre"]
+        booksBinding = booksInfo["Book binding"]
+        print(f"Title - {booksName}")
+        print(f"Author - {authorName}")
+        print(f"Pages - {booksPages}")
+        print(f"Genre - {booksGenre}")
+        print(f"Binding - {booksBinding}")
+
+if choise == 3:
+    for books, booksInfo in library.items():
+        print(books)
+        booksName = booksInfo["Title"]
+        authorName = booksInfo["Author"]
+        booksPages = booksInfo["Pages"]
+        booksGenre = booksInfo["Genre"]
+        booksBinding = booksInfo["Book binding"]
+        print(f"Title - {booksName}")
+        print(f"Author - {authorName}")
+        print(f"Pages - {booksPages}")
+        print(f"Genre - {booksGenre}")
+        print(f"Binding - {booksBinding}")
+    editBook = input("Which book you want to edit[number of book]: ")
+    print(library[f"Book_{editBook}"])
+    editBookContent = input("What you want to change[enter like 'Title']: ")
+    if editBookContent == 'Title':
+        editTitle = input("Enter new title: ")
+        library[f"Book_{editBook}"]["Title"] = editTitle
+        for k, v in library[f"Book_{editBook}"].items():
+            print(k, "-", v)
+    if editBookContent == 'Author':
+        editAuthor = input("Change author: ")
+        library[f"Book_{editBook}"]["Author"] = editAuthor
+        for k, v in library[f"Book_{editBook}"].items():
+            print(k, "-", v)
+    if editBookContent == 'Pages':
+        editPages = input("Change number of pages: ")
+        library[f"Book_{editBook}"]["Pages"] = editPages
+        for k, v in library[f"Book_{editBook}"].items():
+            print(k, "-", v)
+    if editBookContent == 'Genre':
+        editGenre = input("Change genre: ")
+        library[f"Book_{editBook}"]["Genre"] = editGenre
+        for k, v in library[f"Book_{editBook}"].items():
+            print(k, "-", v)
+
+if choise == 4:
+    for i in range(1, len(library) + 1):
+        print("-------------")
+        print(library[f"Book_{i}"]["Title"],
+              "-", library[f"Book_{i}"]["Pages"], "pages")
+    print("-------------")
+    findBook = input(
+        "Whick book you want to see all information[enter number]: ")
+    for k, v in library[f"Book_{findBook}"].items():
+        print(k, "-", v)
