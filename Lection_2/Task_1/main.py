af800cb7a63e72b0ad5a1743f0e8fbc8422c00d3
Kewaaaa <inokenty119228@gmail.com>
2023-04-20 13:23:36 +0300

    Redid the task

--- Lection_2/Task_1/main.py
+++ Lection_2/Task_1/main.py
@@ -1,20 +1,25 @@
 # This program ask some elements and add to list when you push 'Enter'
 # and you can finish program type 'end'
 
+
 def countElem(lst):
     return len(lst)
 
 
 def main():
     bufferList = []
-    num = 1
+    print("Type 'end' to end the program, or 'search' to the search element")
     while True:
-        askElement = input(
-            "Enter some info, or type 'end' to end the program: ")
+        askElement = input("Enter a word: ")
         if askElement == "end":
             break
         bufferList.append(askElement)
-    num += 1
+        if askElement == "search":
+            searchElem = input("Element you want to see: ")
+            if searchElem in bufferList:
+                index = bufferList.index(searchElem)
+                print(bufferList)
+                print(f"{searchElem} - {index + 1} index")
     print("Your list - ", bufferList)
     print("Elements: ", countElem(bufferList))
 
